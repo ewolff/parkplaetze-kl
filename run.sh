4f37ffae93fca7b730a30dee8c0c19fd77716b52
Eberhard Wolff <eberhard.wolff@posteo.net>
2026-06-21 18:38:57 +0200

    Fix Zeitzone

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 # Python Skript erzeugt CSV auf stdout
 curl https://www.kaiserslautern.de/sozial_leben_wohnen/verkehr_parken/autos_und_co/parken/index.html.de > daten.html
-TZ="Europe/Berlin" printf "%s," "$(date '+%Y-%m-%d %H:%M:%S')" >> parkplatzdaten.csv
+export TZ="Europe/Berlin"
+printf "%s," "$(date '+%Y-%m-%d %H:%M:%S')" >> parkplatzdaten.csv
 python3 parkplaetze.py < daten.html >> parkplatzdaten.csv
