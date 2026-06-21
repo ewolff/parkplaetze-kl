85ea550e6e71ff94e31a4ac8ac1386f137cf3730
Eberhard Wolff <eberhard.wolff@posteo.net>
2026-06-21 23:14:32 +0200

    HTML Daten direkt in das Python Skript pipen statt erst in eine Datei schreiben

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Python Skript erzeugt CSV auf stdout
-curl https://www.kaiserslautern.de/sozial_leben_wohnen/verkehr_parken/autos_und_co/parken/index.html.de > daten.html
 export TZ="Europe/Berlin"
 printf "%s," "$(date '+%Y-%m-%d %H:%M:%S')" >> parkplatzdaten.csv
-python3 parkplaetze.py < daten.html >> parkplatzdaten.csv
+curl https://www.kaiserslautern.de/sozial_leben_wohnen/verkehr_parken/autos_und_co/parken/index.html.de | python3 parkplaetze.py >> parkplatzdaten.csv
